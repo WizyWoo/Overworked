e263630ff18369394785f5cc0764cd9a88b2d4c7
Shyronzo <james-sst@hotmail.com>
2022-09-02 17:04:51 +0200

    Basic debugging timelimit to force the player to lose

--- Assets/Scripts/LoseCondition.cs
+++ Assets/Scripts/LoseCondition.cs
@@ -6,9 +6,16 @@ public class LoseCondition : MonoBehaviour
 {
     public bool Game_Over;
     public GameObject LoserHud;
+    public float Timer;
     // Start is called before the first frame update
     void Start()
     {
+        if (Timer == 0)
+        {
+            Timer = 60;
+        }
+            
+
         LoserHud.SetActive(false);
     }
 
@@ -18,8 +25,11 @@ public class LoseCondition : MonoBehaviour
         // if Game_Over is true, sets timescale to 0.001   making the game slower     
         // turning on the LoserHud object containing the Game Over "hud"
         // as a result of this physics will be kinda slow until reset back to 1
-
-
+        Timer -= Time.deltaTime;
+        if(Timer < 0)
+        {
+            Game_Over = true;
+        }
         if (Game_Over == true)
         {
             Time.timeScale = 0.001f;
