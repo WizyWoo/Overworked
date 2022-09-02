ee90eac3c981885a014dd3f333e2f984a4415985
Shyronzo <james-sst@hotmail.com>
2022-09-02 10:17:36 +0200

    Quick Quit and Reset
    they do what they say on the packaging

---
+++ Assets/Scripts/ResetLevel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetLevel : MonoBehaviour
+{
+    public string CurrentSceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Gets the current Scene name, saves it briefly, then reloads that scene. 
+    //attach the void to a button to make it do it's magic.
+   public void Restart_Current_Level()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        scene.name = CurrentSceneName;
+        SceneManager.LoadScene(CurrentSceneName);
+    }
+}
