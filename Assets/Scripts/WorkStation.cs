6a7c875a57e1efb81320a48074d129f088137754
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2022-09-02 16:27:07 +0200

    poggers fixes

--- Assets/Scripts/WorkStation.cs
+++ Assets/Scripts/WorkStation.cs
@@ -8,7 +8,7 @@ public class WorkStation : MonoBehaviour , IInteractable
     public float UseRange, CraftingSpeed;
     public bool InUse;
     public GrabbableItem ItemOnStaion;
-    public Transform OccupiedBy;
+    public Transform OccupiedBy, DisplayPoint;
     private CraftableItem craftingItem;
 
     public void Activate(Transform _player = null, bool _buttonDown = true)
@@ -26,7 +26,7 @@ public class WorkStation : MonoBehaviour , IInteractable
         if(ItemOnStaion)
         {
 
-            if(ItemOnStaion.TryGetComponent<CraftableItem>(out craftingItem))
+            if(craftingItem)
             {
 
                 InUse = _buttonDown;
@@ -45,10 +45,29 @@ public class WorkStation : MonoBehaviour , IInteractable
         else if(_player.GetComponent<PlayerController>().itemGrabbed)
         {
 
-            ItemOnStaion = _player.GetComponent<PlayerController>().itemGrabbed;
+            PlayerController _pC = _player.GetComponent<PlayerController>();
+            ItemOnStaion = _pC.itemGrabbed;
+            _pC.itemGrabbed = null;
+            ItemOnStaion.transform.SetParent(null);
+            ItemOnStaion.transform.position = DisplayPoint.position;
+
+            if(!ItemOnStaion.TryGetComponent<CraftableItem>(out craftingItem))
+                InvalidItem();
+            else if(craftingItem.Assembled)
+                InvalidItem();
 
         }
 
+    }
+
+    public void InvalidItem()
+    {
+
+        craftingItem.UngrabItem();
+        ItemOnStaion = null;
+        OccupiedBy = null;
+        InUse = false;
+        
     }
 
     private void Update()
@@ -68,6 +87,12 @@ public class WorkStation : MonoBehaviour , IInteractable
         {
 
             craftingItem.Progress += CraftingSpeed * Time.deltaTime;
+            if(craftingItem.Progress >= 100)
+            {
+
+                InvalidItem();
+
+            }
 
         }
 
