2ff3df960b552959eb0459634c7e01f75c699ba6
Shyronzo <james-sst@hotmail.com>
2022-09-30 16:23:04 +0200

    make the icecubepng show up when frozen
    :)

--- Assets/Scripts/MechanicImplementation/FreezePlayer.cs
+++ Assets/Scripts/MechanicImplementation/FreezePlayer.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FreezePlayer : MonoBehaviour
 {
     public bool Frozen;
+    public GameObject IceCubePNG;
     private float Timer;
     // Start is called before the first frame update
     void Start()
@@ -21,11 +22,13 @@ public class FreezePlayer : MonoBehaviour
         }
         if (Frozen == false)
         {
+            IceCubePNG.SetActive(false);
             gameObject.GetComponent<PlayerController>().enabled = true;
             Timer = 2;
         }
         if (Frozen == true)
         {
+            IceCubePNG.SetActive(true);
             gameObject.GetComponent<PlayerController>().enabled = false;
             Timer -= Time.deltaTime;
         }
