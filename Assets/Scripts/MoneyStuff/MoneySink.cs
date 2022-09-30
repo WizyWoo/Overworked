00785b3b5958423ef5ea41130360b42e6fab1b53
Shyronzo <james-sst@hotmail.com>
2022-09-30 12:14:36 +0200

    quick cleanup of MoneySink
    moving stuff to start as we dont need it in update, made it actually correct and reflect on our total

--- Assets/Scripts/MoneyStuff/MoneySink.cs
+++ Assets/Scripts/MoneyStuff/MoneySink.cs
@@ -11,13 +11,16 @@ public class MoneySink : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        //we find the text, for each number we want
        MoneyText = GameObject.Find("Money.txt").GetComponent<Text>();
         TaxText = GameObject.Find("Tax").GetComponent<Text>(); 
         DebtText = GameObject.Find("Debt").GetComponent<Text>();    
         RentText = GameObject.Find("Rent").GetComponent<Text>();
         LeftoverMoneyText = GameObject.Find("LeftoverMoney").GetComponent<Text>();
-        
+        //we then say money is the same as the total money from the game manager
         Money = GameManager.instance.TotalMoney;
+        //then if it is exactly zero, we get a new debt, unlikely to happen during normal play but it is possible.
         if (GameManager.instance.TotalDebt == 0 )
         {
             Debt =  Random.Range(5000, 50000);
@@ -25,34 +28,41 @@ public class MoneySink : MonoBehaviour
             GameManager.instance.TotalDebt = ((int)Debt);
             
         }
-        
-        
-        bonusDebt = Debt * 0.01f;
-        GameManager.instance.TotalDebt += ((int)bonusDebt);
-        Debt = GameManager.instance.TotalDebt;
 
+        //we then take 1% of the debt and add it to the total, and further make it crazy huge
+        Debt = GameManager.instance.TotalDebt;
+        bonusDebt = Debt * 0.01f;
+        
+        GameManager.instance.TotalDebt += ((int)bonusDebt);
+        Debt += bonusDebt;
+        //we then update the text here and get numbers for tax, adjust them as needed for balance and to rub it in.
+        MoneyText.text = "Money: " + Money;
+        Tax = Money * 0.1f;
+        TaxText.text = "Tax: " + Tax;
+        DebtText.text = "Debt: " + Debt;
+        
+        Rent = Money * 0.69f; //Nice
+        RentText.text = "Rent: " + Rent;
+        LeftoverMoney = Money - (Tax + Rent);
+        LeftoverMoneyText.text = "After Fees: " + LeftoverMoney;
+        GameManager.instance.TotalMoney -= ((int)Rent + (int)Tax);
     }
 
     // Update is called once per frame
     void Update()
     {
-        MoneyText.text = "Money: " + Money;
-        Tax = Money * 0.1f;
-        TaxText.text = "Tax: " + Tax;
-        DebtText.text = "Debt: " + Debt;
-        RentText.text = "Rent: " + Rent;
-        Rent = Money * 0.69f; //Nice
-        if(PayPal == false)
-        {
-            LeftoverMoney = Money - (Tax + Rent);
-        }
        
-        LeftoverMoneyText.text = "After Fees: " + LeftoverMoney;
+       
+        
     }
+    //we pay off rent here using PayPal, jokes aside it just checks if you have paid the landlord "today"
+    //and if not it doesnt remove what's left of your money
     public void PaidDebt()
     {
         Debt -= LeftoverMoney;
         PayPal = true;
         LeftoverMoney = 0;
+        GameManager.instance.TotalMoney = 0;
+        
     }
 }
