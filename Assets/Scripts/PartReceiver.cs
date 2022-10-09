03cb3fd0f0e3bd7a1d66148b65d39c5c5210ddb8
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2022-10-09 20:27:18 +0200

    forgot to pushhhh

---
+++ Assets/Scripts/PartReceiver.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class PartReceiver : MonoBehaviour
+{
+
+    [SerializeField] Transform platform;
+    [SerializeField] Transform robotSpot;
+
+    [SerializeField] Transform pointA;
+    [SerializeField] Transform pointB;
+    [SerializeField] Level02_Manager level02_Manager;
+
+    CraftableItem currentItem;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CraftableItem item = other.GetComponent<CraftableItem>();
+
+        if (item != null && item.typeOfItem == CraftableItem.TypeOfRepairableItem.bucket && currentItem == null && item.Assembled)
+        {
+            PlayerController pl = item.transform.GetComponentInParent<PlayerController>();
+            if (pl != null)
+            {
+                StartCoroutine(pl.TryRemoveGrabbableItemFromList(item));
+                pl.RemoveItem(item);
+                pl.itemGrabbed = null;
+            }
+            level02_Manager.CorrectRobot();
+            item.GrabItem();
+            item.transform.SetParent(robotSpot);
+            item.transform.DOMove(robotSpot.position, .5f).OnComplete(TakeAwayRobot);
+
+            currentItem = item;
+
+            item.GetComponent<CraftableItem>().delivered = true;
+
+            item.enabled = false;
+        }
+    }
+
+
+    void TakeAwayRobot()
+    {
+        
+        platform.transform.DOMove(pointB.position, 1).OnComplete(PreparePlatfomrForNextRobot);
+    }
+
+    void PreparePlatfomrForNextRobot()
+    {
+        Destroy(currentItem.gameObject);
+        currentItem = null;
+        platform.transform.DOMove(pointA.position, 1);
+    }
+
+}
