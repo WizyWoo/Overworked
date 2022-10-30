bf0fb0b985bd9f34d5ad130d67ddd22fbf681c5b
BrailleBell <Bellingmo.iver@gmail.com>
2022-10-30 14:01:45 +0100

    press indication

--- Assets/Scripts/_Boss_interactions/Dialogue_tool.cs
+++ Assets/Scripts/_Boss_interactions/Dialogue_tool.cs
@@ -5,6 +5,11 @@ using UnityEditor;
 
 public class Dialogue_tool : EditorWindow
 {
+    public string[] tab_names = new string[] { "characters", "options", "dialogue" };
+    public int tab_number = 0;
+
+
+
     [MenuItem("Window/Dialogue")]
     public static void ShowWindow()
     {
@@ -12,7 +17,26 @@ public class Dialogue_tool : EditorWindow
     }
     private void OnGUI()
     {
-        
+        tab_number = GUILayout.Toolbar(tab_number, tab_names);
+
+        switch (tab_number)
+        {
+            case 0: character(); break;
+            case 1: options(); break;
+
+        }
+    }
+
+    public void character()
+    {
+        GUILayout.Label("characters");
+
+
+    }
+
+    public void options()
+    {
+        GUILayout.Label("options");
     }
 
 }
