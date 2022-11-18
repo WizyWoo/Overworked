2563d078eb25b10a4cc4d44242c78a5c8a29f74e
WizyWoo <72544537+WizyWoo@users.noreply.github.com>
2022-11-18 09:56:13 +0100

    fixed some stuff in lvl 6

--- Assets/Scripts/Managers/Level06_Manager.cs
+++ Assets/Scripts/Managers/Level06_Manager.cs
@@ -7,4 +7,20 @@ using TMPro;
 public class Level06_Manager : LevelManager
 {
 
+    [SerializeField]
+    private GameObject bridges;
+
+    protected override void Awake()
+    {
+
+        base.Awake();
+
+        // Setup level for 1 or 2 players
+        if (GameManager.instance.onlyOnePlayer)
+            bridges.SetActive(true);
+        else
+            bridges.SetActive(false);
+
+    }
+
 }
