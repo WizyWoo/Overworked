3d73fb8c439ce02066652a7bec9ea38db76aad69
damart24 <82331644+damart24@users.noreply.github.com>
2022-12-08 04:43:58 +0100

    sound of lava when player falls

--- Assets/Scripts/ItemS/ItemPenaltyTrigger.cs
+++ Assets/Scripts/ItemS/ItemPenaltyTrigger.cs
@@ -7,17 +7,21 @@ public class ItemPenaltyTrigger : MonoBehaviour
 
     public LevelManager ThisLevelsManager;
     public int Penalty;
+    public FMODUnity.EventReference lavaSound;
 
     private void OnTriggerEnter(Collider _col)
     {
 
-        if(_col.TryGetComponent<GrabbableItem>(out GrabbableItem _item))
+        if (_col.TryGetComponent<GrabbableItem>(out GrabbableItem _item))
         {
 
             ThisLevelsManager.UpdateMoney(Penalty);
             Destroy(_item.gameObject);
 
         }
+        else if (_col.GetComponent<PlayerController>())
+            SoundManager.Instance.PlaySound(lavaSound, gameObject);
+
 
     }
 
